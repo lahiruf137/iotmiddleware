8dd4683336559578c99b3da99243cfb7d95a731b
lahiruf137 <lahiruf137@gmail.com>
2019-12-12 19:42:05 +0530

    update

--- iotmiddleware/src/main/java/com/example/iotmiddleware/management/RemoteOperationServer.java
+++ iotmiddleware/src/main/java/com/example/iotmiddleware/management/RemoteOperationServer.java
@@ -4,14 +4,20 @@ import java.net.InetAddress;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.example.iotmiddleware.IotCore;
+
 public class RemoteOperationServer implements Runnable{
+	private static final Logger logger = LoggerFactory.getLogger(RemoteOperationServer.class);
 	public void run() {
 		try {
 			LocateRegistry.createRegistry(1099);
 			Naming.rebind("//"+InetAddress.getLocalHost().getHostAddress()+"/exampleservice", new ServerOperation());
 			
 			}catch (Exception e) {
-				System.out.println(e.getMessage());
+				logger.error("unable to strart remote operations server "+e.getMessage());
 				
 
 			}
