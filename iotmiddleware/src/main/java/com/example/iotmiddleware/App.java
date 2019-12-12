5dc859d9980bd10cc4cb2a12731555a8fb825d61
lahiruf137 <lahiruf137@gmail.com>
2019-12-12 16:54:29 +0530

    update

--- iotmiddleware/src/main/java/com/example/iotmiddleware/App.java
+++ iotmiddleware/src/main/java/com/example/iotmiddleware/App.java
@@ -1,29 +1,47 @@
 package com.example.iotmiddleware;
 
+import java.util.Scanner;
+
+import com.example.iotmiddleware.management.OnEventListener;
 
 /**
 * iotmiddleware
 * 
 */
+
 public class App 
 {
   public static void main( String[] args ) throws Exception
   {
-    IotCore iotsystem = new IotCore();
+    IotCore iotsystem = new IotCore(new OnEventAction() );
     Thread.sleep(500);
     iotsystem.setSelfAttribute("Light_1","on");
     iotsystem.setSelfAttribute("Light_2","off");
     
-    for(int i =0;i<10; i++) {
-    Thread.sleep(2000);
-    for(String s: iotsystem.getNeighbours()){
-      System.out.println("Neighbour : "+s);
-      for (String attr: iotsystem.getNeighbourAttributes(s)) {
-    	  System.out.println(attr+" : "+iotsystem.getNeighbourAttributeValue(s,attr));
-      }
-    }    }
-    System.exit(0);
+    while(true) {
+    	System.out.println("Enter to view neighbours or Ctrl+c to exit");
+    	new Scanner(System.in).nextLine();
+    	printNeighbourInfo(iotsystem);
+    }
+    
   }
   
+  public static void printNeighbourInfo(IotCore iotsystem) throws Exception {
+	  for(String s: iotsystem.getNeighbours()){
+	      System.out.println("Neighbour : "+s);
+	      for (String attr: iotsystem.getNeighbourAttributes(s)) {
+	    	  System.out.println(attr+" : "+iotsystem.getNeighbourAttributeValue(s,attr));
+	      }
+	  } 
+  }
   
+}
+
+class OnEventAction implements OnEventListener{
+	public void onAttributeSet(String key, String value) {
+		System.out.println("Attribuet set "+key+":"+value);
+	}
+	public void onAttributeUnset(String key) {
+		System.out.println("Attribute unset"+key);
+	}
 }
