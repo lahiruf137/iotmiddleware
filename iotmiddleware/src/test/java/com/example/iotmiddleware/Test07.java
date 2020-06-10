2076a9f8ff02f57a1c95baad36039917eb477a41
lahiruf137 <lahiruf137@gmail.com>
2020-06-10 13:19:30 +0530

    update

--- iotmiddleware/src/test/java/com/example/iotmiddleware/Test07.java
+++ iotmiddleware/src/test/java/com/example/iotmiddleware/Test07.java
@@ -7,11 +7,11 @@ import junit.framework.TestCase;
 
 public class Test07 extends TestCase {
 	
-	public void testSetNeighbourAttributeValue() {
+	public void testSetNeighbourAttributeValue() throws Exception {
 		try {
 			IotCore instance1=new IotCore();
 			instance1.setSelfAttribute("mock_attribute", "mock_value");
-			
+			Thread.sleep(7000);
 			instance1.setNeighbourAttribute(InetAddress.getLocalHost().getHostAddress().toString(),"mock_attribute","new_value");
 			String value=instance1.getNeighbourAttributeValue(InetAddress.getLocalHost().getHostAddress().toString(),"mock_attribute");
 			assertEquals(value,"new_value");
